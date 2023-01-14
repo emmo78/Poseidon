3ef73ed95f15a3dd3789880e9c4f14e88d5dc496
OlivierMOREL <oliviermorel.oc1@gmail.com>
2023-01-15 00:12:56 +0100

    wip : CurvePoint coded with validation constraint annotations

--- src/main/java/com/poseidoninc/poseidon/domain/Rating.java
+++ src/main/java/com/poseidoninc/poseidon/domain/Rating.java
@@ -3,10 +3,26 @@ package com.poseidoninc.poseidon.domain;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 @Entity
+@DynamicInsert
+@DynamicUpdate
+@Getter
+@Setter
 @Table(name = "rating")
 public class Rating {
-    // TODO: Map columns in data table RATING with corresponding java fields
+	
+	Integer id ;
+	String moodysRating ;
+	String sandPRating ;
+	String fitchRating ;
+	Integer orderNumber ;
 }
