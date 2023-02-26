10a6c7a0f4ae4d0922907dabf276a06333b2b835
Olivier MOREL <oliviermorel.oc1@gmail.com>
2023-02-26 21:44:24 +0100

    wip : TU getCurvePoints coded

--- src/test/java/com/poseidoninc/poseidon/services/CurvePointServiceTest.java
+++ src/test/java/com/poseidoninc/poseidon/services/CurvePointServiceTest.java
@@ -2,12 +2,15 @@ package com.poseidoninc.poseidon.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.AfterAll;
@@ -24,6 +27,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -152,4 +158,95 @@ public class CurvePointServiceTest {
 				.getMessage()).isEqualTo("Error while getting curvePoint");
 		}
 	}
+	
+	@Nested
+	@Tag("getCurvePointsTests")
+	@DisplayName("Tests for getting curvePoints")
+	@TestInstance(Lifecycle.PER_CLASS)
+	class GetCurvePointsTests {
+		
+		private Pageable pageRequest;
+		
+		@BeforeAll
+		public void setUpForAllTests() {
+			pageRequest = Pageable.unpaged();
+			requestMock = new MockHttpServletRequest();
+			requestMock.setServerName("http://localhost:8080");
+			requestMock.setRequestURI("/curvePoint/getCurvePoints");
+			request = new ServletWebRequest(requestMock);
+		}
+
+		@AfterAll
+		public void unSetForAllTests() {
+			pageRequest = null;
+			requestMock = null;
+			request = null;
+		}
+		
+		@AfterEach
+		public void unSetForEachTests() {
+			curvePointService = null;
+			curvePoint = null;
+		}
+
+		@Test
+		@Tag("CurvePointServiceTest")
+		@DisplayName("test getCurvePoints should return curvePoints")
+		public void getCurvePointsTesthouldRetrunExcpectedCurvePoints() {
+			
+			//GIVEN
+			List<CurvePoint> expectedCurvePoints = new ArrayList<>();
+			curvePoint = new CurvePoint();
+			curvePoint.setId(1);
+			curvePoint.setCurveId(2);
+			curvePoint.setAsOfDate(LocalDateTime.parse("21/01/2023 10:20:30", DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
+			curvePoint.setTerm(3.0);
+			curvePoint.setValue(4.0);
+			curvePoint.setCreationDate(LocalDateTime.parse("22/01/2023 12:22:32", DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
+			expectedCurvePoints.add(curvePoint);
+			CurvePoint curvePoint2 = new CurvePoint();			
+			curvePoint2.setId(2);
+			curvePoint2.setCurveId(3);
+			curvePoint2.setAsOfDate(LocalDateTime.parse("23/01/2023 10:20:30", DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
+			curvePoint2.setTerm(4.0);
+			curvePoint2.setValue(5.0);
+			curvePoint2.setCreationDate(LocalDateTime.parse("24/01/2023 12:22:32", DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
+
+			expectedCurvePoints.add(curvePoint2);
+			when(curvePointRepository.findAll(any(Pageable.class))).thenReturn(new PageImpl<CurvePoint>(expectedCurvePoints, pageRequest, 2));
+			
+			//WHEN
+			Page<CurvePoint> resultedCurvePoints = curvePointService.getCurvePoints(pageRequest, request);
+			
+			//THEN
+			assertThat(resultedCurvePoints).containsExactlyElementsOf(expectedCurvePoints);
+		}
+		
+		@Test
+		@Tag("CurvePointServiceTest")
+		@DisplayName("test getCurvePoints should throw UnexpectedRollbackException on NullPointerException")
+		public void getCurvePointsTestShouldThrowsUnexpectedRollbackExceptionOnNullPointerException() {
+			//GIVEN
+			when(curvePointRepository.findAll(any(Pageable.class))).thenThrow(new NullPointerException());
+			//WHEN
+			//THEN
+			assertThat(assertThrows(UnexpectedRollbackException.class,
+					() -> curvePointService.getCurvePoints(pageRequest, request))
+					.getMessage()).isEqualTo("Error while getting CurvePoints");
+		}
+		
+		@Test
+		@Tag("CurvePointServiceTest")
+		@DisplayName("test getCurvePoints should throw UnexpectedRollbackException on any RuntimeException")
+		public void getCurvePointsTestShouldThrowsUnexpectedRollbackExceptionOnAnyRuntimeException() {
+			//GIVEN
+			when(curvePointRepository.findAll(any(Pageable.class))).thenThrow(new RuntimeException());
+			//WHEN
+			//THEN
+			assertThat(assertThrows(UnexpectedRollbackException.class,
+					() -> curvePointService.getCurvePoints(pageRequest, request))
+					.getMessage()).isEqualTo("Error while getting CurvePoints");
+		}
+	}
+
 }
