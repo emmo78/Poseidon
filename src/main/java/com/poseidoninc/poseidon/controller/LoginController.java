3408d487b1150bfd9253e42e8655c1e4b94fcf05
OlivierMOREL <oliviermorel.oc1@gmail.com>
2024-04-17 12:23:42 +0200

    fix : misspelled home path in error templates

--- src/main/java/com/poseidoninc/poseidon/controller/LoginController.java
+++ src/main/java/com/poseidoninc/poseidon/controller/LoginController.java
@@ -1,12 +1,9 @@
 package com.poseidoninc.poseidon.controller;
 
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.security.Principal;
 
 @Controller
 public class LoginController {
@@ -17,14 +14,5 @@ public class LoginController {
             return "redirect:/home";
         }
         return "login";
-    }
-
-    @GetMapping("/error")
-    public ModelAndView error() {
-        ModelAndView mav = new ModelAndView();
-        String errorMessage= "You are not authorized for the requested data.";
-        mav.addObject("errorMsg", errorMessage);
-        mav.setViewName("403");
-        return mav;
     }
 }
